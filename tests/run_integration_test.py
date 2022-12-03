00c68e0b92eb9544bd67139827034bff2cad4341
PApostol <pav.apostolidis@gmail.com>
2022-12-03 16:39:26 +0000

    blue coding style

---
+++ tests/run_integration_test.py
@@ -0,0 +1,26 @@
+"""Simple pytypos integration test"""
+import pprint
+
+from pytypos import Pytypos
+
+typos_py = Pytypos(target='tests/resources/pyfile.py', suggestions=True)
+typos_py.find_typos()
+# typos_py.fix_typos()
+
+typos_rst = Pytypos(
+    target='tests/resources/', recursive=True, match_identifier='', file_extension='rst', exclude_word_list=['RST']
+)
+typos_rst.find_typos()
+# typos_rst.fix_typos()
+
+pp = pprint.PrettyPrinter()
+pp.pprint(typos_py.typo_details)
+pp.pprint(typos_rst.typo_details)
+
+"""
+Output:
+{'resources/pyfile.py': [{'commetn': ['comment', 'comet']},
+                         {'anothr': ['another', 'anthrop']}]}
+
+{'tests/resources/foo.rst': ['cdoe', 'proply']}
+"""
